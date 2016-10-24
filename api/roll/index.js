29242f46cdd946a794dde87081c9558e18a9fe76
Sergio Rodriguez <oneandprime@gmail.com>
2016-10-24 10:39:31 -0400

    Added Read Roll by _id Endpoint

--- api/roll/index.js
+++ api/roll/index.js
@@ -2,40 +2,42 @@
 const express = require('express');
 const Roll = require('roll');
 const DbRoll = require('../../db/Roll');
+const DbUser = require('../../db/User');
 const router = express.Router({
     mergeParams: true
 });
-let io;
 
+router.get('/roll', function(req, res) {
+    let sort = (req && req.sort && req.sort.field) || 'date';
+    let sortDirection = (req && req.sort && req.sort.direction) || '-';
+    let limit = (req && req.limit) || 30;
 
-router.get('/roll/:diceString', function(req, res) {
-    const roll = new Roll();
-    let diceString = req.params.diceString;
-    if (roll.validate(diceString)) {
-        let result = roll.roll(diceString);
-        let newRoll = {
-            _user: req.user && req.user._id,
-            diceString: diceString,
-            result: JSON.stringify(result),
-            date: Date.now()
-        }
-        let db = new DbRoll(newRoll);
-        db.save();
-
-        res.json(newRoll);
-    } else {
-        res.json({
-            error: 'error'
+    DbRoll.find({})
+        .populate('_user', 'email username')
+        .sort(sortDirection + sort)
+        .limit(limit)
+        .exec((err, rolls) => {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rolls);
+            }
         });
-    }
 });
 
-router.get('/roll/validate/:diceString', function(req, res) {
+router.get('/roll/:id', function(req, res) {
     const roll = new Roll();
-    let diceString = req.params.diceString
-    res.json({
-        valid: roll.validate(diceString)
-    });
+    let id = req.params.id;
+
+    DbRoll.findOne({_id: id})
+        .populate('_user', 'email username')
+        .exec((err, roll) => {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(roll);
+            }
+        });
 });
 
 router.post('/roll/', function(req, res) {
@@ -59,10 +61,15 @@ router.post('/roll/', function(req, res) {
                     error: err
                 });
             } else {
-                roll._user = req.user;
+                DbUser.find({
+                    _id: roll._user
+                }).exec((err, usr) => {
+                    roll._user = usr;
+                    req.app.io.emit('roll', roll);
+                    res.json(roll);
 
-                req.app.io.emit('roll', roll);
-                res.json(roll);
+                });
+
             }
         });
 
@@ -73,22 +80,12 @@ router.post('/roll/', function(req, res) {
     }
 });
 
-router.get('/roll', function(req, res) {
-    let sort = (req && req.sort && req.sort.field) || 'date';
-    let sortDirection = (req && req.sort && req.sort.direction) || '-';
-    let limit = (req && req.limit) || 30;
-
-    DbRoll.find({})
-        .populate('_user', 'email username')
-        .sort(sortDirection + sort)
-        .limit(limit)
-        .exec((err, rolls) => {
-            if (err) {
-                res.json(err);
-            } else {
-                res.json(rolls);
-            }
-        });
+router.get('/roll/validate/:diceString', function(req, res) {
+    const roll = new Roll();
+    let diceString = req.params.diceString
+    res.json({
+        valid: roll.validate(diceString)
+    });
 });
 
 module.exports = router;
